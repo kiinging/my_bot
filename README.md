85613e68835aa7830fc72ac330ee41905dd2b8ad
kiinging <wongkiinging@gmail.com>
2024-06-18 17:22:55 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# my_bot
+
+echo "# my_bot" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/kiinging/my_bot.git
+git push -u origin main
---
+++ launch/old.launch_sim.launch.py
@@ -0,0 +1,43 @@
+# this launch file works for gazebo_control
+# the new launch_sim.lauch.py is for ros2_control
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    package_name = 'my_bot'  # Ensure this matches your package name
+
+    # Include the robot_state_publisher launch file
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # Include the Gazebo launch file
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+    )
+
+    # Run the spawner node to spawn the entity
+    spawn_entity = Node(
+        package='gazebo_ros', executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'my_bot'],
+        output='screen'
+    )
+
+    
+
+    # Launch description
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity
+    ])
+
