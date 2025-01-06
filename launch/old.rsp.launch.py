b8de0dedc8abfcfe7d0e2d947bccefe1d75620d9
kiinging <wongkiinging@gmail..com>
2025-01-06 19:25:38 +0800

    my_bot update

--- launch/old.rsp.launch.py
+++ launch/old.rsp.launch.py
@@ -1,4 +1,3 @@
-# old file
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -30,6 +29,16 @@ def generate_launch_description():
         parameters=[params]
     )
 
+     # Add RViz2 node
+    rviz_config_file = os.path.join(pkg_path, 'config', 'my_bot.rviz')  # Path to RViz2 config file
+    node_rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_file],  # Pass the RViz2 config file
+        parameters=[{'use_sim_time': use_sim_time}]
+    )
+
 
     # Launch!
     return LaunchDescription([
@@ -38,5 +47,6 @@ def generate_launch_description():
             default_value='false',
             description='Use sim time if true'),
 
-        node_robot_state_publisher
+        node_robot_state_publisher,
+        node_rviz2
     ])
